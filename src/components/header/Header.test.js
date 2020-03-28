53884e4abc97b4a8d5bb303ce2e249908bd086a8
Jeffery Miller <jeffery.f@gmail.com>
2020-03-28 13:49:21 -0500

    Updates header components and adds tests

---
+++ src/components/header/Header.test.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { findByTestAttr } from '../../../Utils';
+import renderer from 'react-test-renderer';
+import Header from './Header';
+
+const setUp = () => {
+  const wrapper = shallow(<Header />);
+  return wrapper;
+}
+
+describe('Header Component', () => {
+  let wrapper;
+  
+  beforeEach(() => {
+    wrapper = setUp();
+  });
+  
+  describe('snapshot tests', () => {
+    it('renders correctly', () => {
+      const tree = renderer
+        .create(wrapper)
+        .toJSON();
+  
+      expect(tree).toMatchSnapshot();
+    });
+
+    // Personally, I think that snapshot tests are better for this
+    // as it's kind of busywork, but these are defintely easier to 
+    // reason about/read.
+    describe('wrapper tests', () => {
+      it('renders the logo correctly', () => {
+        expect(findByTestAttr(wrapper, 'logoImg').length).toBe(1);
+      });
+
+      it('renders the headerComponent correctly', () => {
+        expect(findByTestAttr(wrapper, 'headerComponent').length).toBe(1);
+      });
+    });
+  });
+});
